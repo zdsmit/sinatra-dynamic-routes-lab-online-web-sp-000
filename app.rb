88613d64169b9128db4879520ff777228c1a2a0d
Zach  Smith <zdsmith001@gmail.com>
2019-11-15 21:45:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @name = params[:name].reverse!
+    "#{@name}"
+  end
+
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number**2}"
+  end
+
+  get "/say/:number/:phrase" do
+    string = ' '
+    params[:number].to_i.times do
+      string += params[:phrase]
+    end
+    string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @one = params[:word1]
+    @two = params[:word2]
+    @three = params[:word3]
+    @four = params[:word4]
+    @five = params[:word5]
+    "#{@one} #{@two} #{@three} #{@four} #{@five}."
+  end
+
+  get "/:operation/:number1/:number2" do
+    if params[:operation] == "add"
+      @operator = "+"
+    elsif params[:operation] == "subtract"
+      @operator = "-"
+    elsif params[:operation] == "multiply"
+      @operator = "*"
+    elsif params[:operation] == "divide"
+      @operator = "/"
+    end
+    eval"#{params[:number1].to_i} #{@operator} #{params[:number2].to_i}"
+  end
 
 end
